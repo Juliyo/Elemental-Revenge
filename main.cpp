9f878aee1466a340a28818842b8bb2759008f007
Toni <tonire.rebollo@gmail.com>
2016-03-19 19:55:15 +0100

    YA

--- main.cpp
+++ main.cpp
@@ -22,7 +22,11 @@ void info(){
     std::cout << "  @.  @.  `@       @ @    @`     @    @@@  @+     @  @    " << std::endl;
     std::cout << "  @:   @' .@  :    :@:    @   ;  @     @@   @@:  +@  @    " << std::endl;
     std::cout << "  +:    @ `@@@@     #     @@@@@ .@      #     ;@@:   @@@@'" << std::endl;
-    
+    std::cout << "------------------------------------------------------------------------"<<std::endl;
+    std::cout << ""<<std::endl;
+    std::cout << "Version: Hito 1" <<std::endl;
+    std::cout << "Rama: Flash Entregable" <<std::endl;
+    std::cout << "Instrucciones: Pulsa la tecla E para teletransportarte hacia \nla posicion del mouse." <<std::endl;
 }
 
 int main(int, char const**) {
