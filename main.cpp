1b00037e9a96722bd782933ed0b300273dc860b5
Toni <tonire.rebollo@gmail.com>
2016-03-21 17:12:13 +0100

    Main actualizado

--- main.cpp
+++ main.cpp
@@ -1,27 +1,18 @@
 #include "../Headers/Game.hpp"
 void info(){
     std::cout << "" << std::endl;
-    std::cout << "  @@@@; +`    ;@@@@; :.       @  ;@@@@; ,`      @ @@@@@@@`  @     @  " << std::endl;
-    std::cout << "  @     @;    .@  `, ;@      @@  .@  `, ;@      @ :  @      @.    @  " << std::endl;
-    std::cout << "  @     @:     @     :#@     @@   @      @@     @    @     `+@    @  " << std::endl;
-    std::cout << "  @     @:     @     :`@    @`@   @      ++@    @    @     @ @    @  " << std::endl;
-    std::cout << "  @@@@  #.     @@@@  ; ;@   @ @   @@@@   + @@   @    @     @ ;#   #  " << std::endl;
-    std::cout << "  @     #.     @     #  @  @  @   @      #  @#  @    @    @   @   #  " << std::endl;
-    std::cout << "  @     @`     @     @  ;@ @  @   @      #   @'`@    @    @@@@@:  @  " << std::endl;
-    std::cout << "  @     @`    `@     @   @@   @,  @      @    @:@    @   @.   .@  @  " << std::endl;
-    std::cout << "  @     @`    .@     @   +@   @@  @      @     @@    @   @     @. @   #" << std::endl;
-    std::cout << "  @@@@  @@@@@ `@@@@  @        ;+  @@@@   ;      @    @   @     `: @@@@@" << std::endl;
+    std::cout << "    ________                          __        __" << std::endl;
+    std::cout << "   / ____/ /__  ____ ___  ___  ____  / /_____ _/ /" << std::endl;
+    std::cout << "  / __/ / / _ \\/ __ `__ \\/ _ \\/ __ \\/ __/ __ `/ / " << std::endl;
+    std::cout << " / /___/ /  __/ / / / / /  __/ / / / /_/ /_/ / /  " << std::endl;
+    std::cout << "/_____/_/\\___/_/ /_/ /_/\\___/_/ /_/\\__/\\__,_/_/  " << std::endl;
     std::cout << std::endl;
-    std::cout << "  :@@#    ;@@@@;.:     @` @@@@@  ;     .@     `@@`   @@@@@" << std::endl;
-    std::cout << "  @; ;@`  .@  `, @     @; @:  :  @+     @   .@+      @    " << std::endl;
-    std::cout << "  @.  '@   @     @:    @. @.     @@,    @  ,@        @    " << std::endl;
-    std::cout << "  #`  :@   @     .@    @  #.     @`@`   @  @         @    " << std::endl;
-    std::cout << "  #`  @:   @@@@   @   `@  #@@@@  @ .@   @ .@         @@@@:" << std::endl;
-    std::cout << "  #`.@+    @      #;  @   #`     @  ;@  @ :@     +@  @    " << std::endl;
-    std::cout << "  @`:@.    @       @ `#   @`     @   #@ @ `@      @  @    " << std::endl;
-    std::cout << "  @.  @.  `@       @ @    @`     @    @@@  @+     @  @    " << std::endl;
-    std::cout << "  @:   @' .@  :    :@:    @   ;  @     @@   @@:  +@  @    " << std::endl;
-    std::cout << "  +:    @ `@@@@     #     @@@@@ .@      #     ;@@:   @@@@'" << std::endl;
+    std::cout << "    ____                                " << std::endl;
+    std::cout << "   / __ \\___ _   _____  ____  ____ ____ " << std::endl;
+    std::cout << "  / /_/ / _ \\ | / / _ \\/ __ \\/ __ `/ _ \\" << std::endl;
+    std::cout << " / _, _/  __/ |/ /  __/ / / / /_/ /  __/" << std::endl;
+    std::cout << "/_/ |_|\\___/|___/\\___/_/ /_/\\__, /\\___/ " << std::endl;
+    std::cout << "                           /____/       " << std::endl;
     std::cout << "------------------------------------------------------------------------"<<std::endl;
     std::cout << ""<<std::endl;
     std::cout << "Version: Hito 1" <<std::endl;
