3cdbdc39cd404e1116a7697de21c6171669379d5
Jose Luis Muñoz <jose_lu95@hotmail.es>
2016-04-06 09:38:02 +0200

    commit auxiliar

---
+++ Headers/Menu.hpp
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Menu.hpp
+ * Author: joselu
+ *
+ * Created on 6 de abril de 2016, 9:37
+ */
+
+#ifndef MENU_HPP
+#define MENU_HPP
+
+class Menu {
+public:
+    Menu();
+    Menu(const Menu& orig);
+    virtual ~Menu();
+private:
+
+};
+
+#endif /* MENU_HPP */
+
---
+++ Menu.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Menu.cpp
+ * Author: joselu
+ * 
+ * Created on 6 de abril de 2016, 9:37
+ */
+
+#include "Menu.hpp"
+
+Menu::Menu() {
+}
+
+Menu::Menu(const Menu& orig) {
+}
+
+Menu::~Menu() {
+}
+
